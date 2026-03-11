1d34b92000f185fe3e9e0e97fb616a756e5c9cf1
Brody Funk <brodfunk@iu.edu>
2026-03-11 16:45:05 -0400

    final

--- loader.py
+++ loader.py
@@ -21,21 +21,34 @@ def fetch_location_data(geolocator, loc):
     location = geolocator.geocode(loc)
 
     if location is None:
-        return None
-    
-    return {"location": loc, "latitude": location.latitude, "longitude": location.longitude, "type": location.geo_type}
+        return {
+            "location": loc,
+            "latitude": pd.NA,
+            "longitude": pd.NA,
+            "type": pd.NA
+        }
+
+    return {
+        "location": loc,
+        "latitude": location.latitude,
+        "longitude": location.longitude,
+        "type": location.raw.get("type", pd.NA)
+    }
 
 def build_geo_dataframe(locations):
-    geo_data = [fetch_location_data(geolocator, loc) for loc in locations]
+    geolocator = get_geolocator()
+    geo_data = [fetch_location_data(get_geolocator(), loc) for loc in locations]
     
     return pd.DataFrame(geo_data)
 
 
 if __name__ == "__main__":
-    geo = get_geolocator()
-
-    locations = ["Museum of Modern Art", "iuyt8765(*&)", "Alaska", "Franklin's Barbecue", "Burj Khalifa"]
+    locations = [
+        "Museum of Modern Art",
+        "iuyt8765(*&)",
+        "Alaska",
+        "Franklin's Barbecue",
+        "Burj Khalifa"
+    ]
 
     df = build_geo_dataframe(locations)
-
-    df.to_csv("./geo_data.csv")
--- test_loader.py
+++ test_loader.py
@@ -4,15 +4,38 @@ from loader import *
 
 class TestLoader(unittest.TestCase):
     def test_valid_locations(self):
-        # your code here
-        return None
+        geolocator = get_geolocator()
+
+        museum = fetch_location_data(geolocator, "Museum of Modern Art")
+        park = fetch_location_data(geolocator, "USS Alabama Battleship Memorial Park")
+
+        self.assertEqual(museum["location"], "Museum of Modern Art")
+        self.assertAlmostEqual(museum["latitude"], 40.7618552, places=2)
+        self.assertAlmostEqual(museum["longitude"], -73.9782438, places=2)
+        self.assertEqual(museum["type"], "museum")
+
+        self.assertEqual(park["location"], "USS Alabama Battleship Memorial Park")
+        self.assertAlmostEqual(park["latitude"], 30.684373, places=2)
+        self.assertAlmostEqual(park["longitude"], -88.015316, places=2)
+        self.assertEqual(park["type"], "park")
 
     def test_invalid_location(self):
         geolocator = get_geolocator()
         result = fetch_location_data(geolocator, "asdfqwer1234")
 
-        self.assertIsNone(result, 
-                          "A nonexistent location should have an empty result.")
+        self.assertEqual(result["location"], "asdfqwer1234")
+        self.assertTrue(pd.isna(result["latitude"]))
+        self.assertTrue(pd.isna(result["longitude"]))
+        self.assertTrue(pd.isna(result["type"]))
+
+def test_invalid_location(self):
+    geolocator = get_geolocator()
+    result = fetch_location_data(geolocator, "asdfqwer1234")
+
+    self.assertEqual(result["location"], "asdfqwer1234")
+    self.assertTrue(pd.isna(result["latitude"]))
+    self.assertTrue(pd.isna(result["longitude"]))
+    self.assertTrue(pd.isna(result["type"]))
 
 if __name__ == "__main__":
     unittest.main()
